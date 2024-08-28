bed43c933c66a615270c237aae6796cbdb225200
Daniel Tull <dt@danieltull.co.uk>
2024-08-28 15:57:40 +0200

    Add starting colors picker

---
+++ TODO/Task 3 - Kids (sections) (identical to QuadColors in Reference)/QuadColors/StartingColorsPicker.swift
@@ -0,0 +1,29 @@
+import SwiftUI
+
+struct StartingColors {
+    var topLeft: Color = .red
+    var topRight: Color = .blue
+    var bottomLeft: Color = .green
+    var bottomRight: Color = .yellow
+}
+
+struct StartingColorsPicker: View {
+
+    @Binding var startingColors: StartingColors
+
+    var body: some View {
+        VStack {
+            HStack {
+                ColorPicker("Top Left", selection: $startingColors.topLeft)
+                Spacer()
+                ColorPicker("Top Right", selection: $startingColors.topRight)
+            }
+            Spacer()
+            HStack {
+                ColorPicker("Bottom Left", selection: $startingColors.bottomLeft)
+                Spacer()
+                ColorPicker("Bottom Right", selection: $startingColors.bottomRight)
+            }
+        }
+    }
+}
